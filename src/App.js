1e9bdec8374be49115e96d6c8f9944b423a292f5
petrmrkvicka <petr@petrmrkvicka.com>
2019-04-06 14:12:49 +0200

    free state - styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React, { Component} from "react";
-//import "./App.css";
+import "./App.css";
 
 class App extends Component{
   render(){
     return(
       <div className="App">
-        <h1> Hello, World! </h1>
+        <div className="header">
+          <img src="https://www.zitcom.dk/img/logo/zg-logo.svg" className="logo"/>
+          <p>Some app name</p>
+        </div>
+        <div className="person">
+          <div className="userPicture">
+            <img src="https://www.scannet.dk/img/pictures/employees/soren-lorentzen-lg@2x.jpg" />
+          </div>
+          <h1> Søren Lorentzen </h1>
+        </div>
+
       </div>
     );
   }
