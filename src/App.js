68fe147a92fa64d07237f03ba223e0237692d71a
Aina Linn Georges <ainalinngeorges@Ainas-MacBook-Pro.local>
2019-04-06 19:48:18 +0200

    deleted commented code

--- src/App.js
+++ src/App.js
@@ -6,35 +6,6 @@ import PoweredBy from "./components/PoweredBy.js";
 import TypePersonaContainer from "./components/TypePersonaContainer.js";
 
 class App extends Component {
-    // constructor(props) {
-    //     super(props);
-    //     this.state = {percentage: 0, 
-    //                   speed: 0.0};
-    //     this.startCounter = this.startCounter.bind(this); 
-    //     this.testConnection = this.testConnection.bind(this);
-    // }
-
-    // tick() {
-    //   if (this.state.percentage <= 0) {
-    //     clearInterval(this.timerID);
-    //     return;
-    //   }
-    //   this.setState(function(state) {
-    //     return {
-    //       percentage: state.percentage - state.speed
-    //     };
-    //   });
-    // }
-
-    // // duration is the duration in seconds of the new deep work session
-    // startCounter(duration) {
-    //   this.setState({
-    //     percentage: 100,
-    //     speed: 10 / duration
-    //   });
-    //   this.timerID = setInterval(() => this.tick(), 100);
-    // }
-
     render() {
     //navigator.userAgent === "TODO add raspberry pi user agent"
       if (true) {
