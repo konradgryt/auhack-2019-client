e5ff3102c2af645135617aaeff324da0ab9cf4aa
Konrad Gryt <konrad@vembyte.dk>
2019-04-06 13:52:06 +0200

    checking for os

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
 import React, { Component} from "react";
+import Websocket from 'react-websocket';
 //import "./App.css";
 
-class App extends Component{
-  render(){
+var OSName="Unknown OS";
+if (navigator.appVersion.indexOf("Win")!=-1) OSName="Windows";
+if (navigator.appVersion.indexOf("Mac")!=-1) OSName="MacOS";
+if (navigator.appVersion.indexOf("X11")!=-1) OSName="UNIX";
+if (navigator.appVersion.indexOf("Linux")!=-1) OSName="Linux";
+
+class App extends Component {
+    
+    constructor(props) {
+        super(props);
+        console.log(OSName)
+    }
+
+    testConnection() {
+        console.log("Listening")
+    }
+
+    render() {
+
     return(
-      <div className="App">
+        <div className="App">
         <h1> Hello, World! </h1>
-      </div>
+        <Websocket url='wss://echo.websocket.org/' onOpen={this.testConnection}/>
+        </div>
     );
   }
 }
