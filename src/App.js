0f19b21f23fce49bfa1649d34804b5e23d3bc953
petrmrkvicka <petr@petrmrkvicka.com>
2019-04-06 17:24:41 +0200

    progressbar adjustments

--- src/App.js
+++ src/App.js
@@ -26,52 +26,53 @@ class App extends Component {
     }
 
     render() {
-        if (OSName="MacOS") {
+
+       if (OSName="MacOS") {
           return(
             <div className="App">
             <h1> Web part</h1>
             <Websocket url='wss://echo.websocket.org/' onOpen={this.testConnection}/>
             </div>
         );
-        } else if (OSName="UNIX") {
-            return(
-              <div className="pi-App">
-                <PiHeader />
-                <div className="pi--person">
+      } else if (OSName="UNIX") {
+                return(
+                  <div className="pi-App">
+                    <PiHeader />
+                    <div className="pi--main">
+                      <CircularProgressbar
+                        percentage={80}
+                        className="pi--progressbar"
+                        strokeWidth={10}
+                        backgroundPadding={3}
+                        background="true"
+                        styles={{
+                          path: {
+                            // Path color
+                            stroke: `rgba(255,255,255,0.4)`,
+                            // Customize transition animation
+                            transition: 'stroke-dashoffset 0.5s ease 0s',
+                          },
+                          trail: {
+                            // Trail color
+                            stroke: 'transparent',
+                          },
+                          background: {
+                            // Trail color
+                            fill: 'rgba(255,255,255,0.2)',
+                          },
 
 
-                <CircularProgressbar
-                  percentage={80}
-                  className="pi--progressbar"
-                  strokeWidth={10}
-                  backgroundPadding={3}
-                  background="true"
-                  styles={{
-                    path: {
-                      // Path color
-                      stroke: `rgba(255,255,255,0.4)`,
-                      // Customize transition animation
-                      transition: 'stroke-dashoffset 0.5s ease 0s',
-                    },
-                    trail: {
-                      // Trail color
-                      stroke: 'transparent',
-                    },
-                    background: {
-                      // Trail color
-                      fill: 'rgba(255,255,255,0.2)',
-                    },
+                        }}
+                      />
+                      <div className="pi--person">
+                        <PiUserImage source="https://www.scannet.dk/img/pictures/employees/soren-lorentzen-lg@2x.jpg" />
+                        <PiUserName name="Søren Lorentzen" />
+                      </div>
+                    </div>
+                  </div>
+                );
+            }
 
-
-                  }}
-                />
-
-                  <PiUserImage source="https://www.scannet.dk/img/pictures/employees/soren-lorentzen-lg@2x.jpg" />
-                  <PiUserName name="Søren Lorentzen" />
-                </div>
-              </div>
-            );
-        }
   }
 }
 
