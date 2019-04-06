e69619443fe773ad86a86202ce1ff710d50cc425
Aina Linn Georges <ainalinngeorges@Ainas-MacBook-Pro.local>
2019-04-07 00:57:21 +0200

    merge

--- src/views/WebView.js
+++ src/views/WebView.js
@@ -3,20 +3,49 @@ import PersonaToggle from '../components/PersonaToggle.js';
 import Text from '../components/Text.js';
 import DashboardControl from "../components/DashboardControl.js"
 import PoweredBy from "./../components/PoweredBy.js";
+import * as moment from 'moment';
 
 
 class WebView extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { time: moment().add(10,'seconds') }; // change this to the default start time
 
-    render() {
-        return(
-          <div className="web-app">
+    this.testTime = this.testTime.bind(this);
+  }
 
-            <DashboardControl />
-            <PoweredBy companyName="Zitcom" />
-
-          </div>
-        );
+  tick() {
+    if (moment().isAfter(this.state.time)) {
+      clearInterval(this.timerID);
+      console.log("test");
+      // send request to start deep work
+      // notification
+      // change view
     }
+  }
+
+  componentDidMount() {
+    this.interval = setInterval(() => this.tick(), 1000);
+  }
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  testTime() {
+    console.log(this.state);
+  }
+
+  render() {
+      this.testTime();
+      return(
+        <div className="web-app">
+
+          <DashboardControl />
+          <PoweredBy companyName="Zitcom" />
+
+        </div>
+      );
+  }
 }
 
 export default WebView;
