f270665bfc4b781fe5c21d86b8de13bab4fa45a5
Nathália Novais <nathnovais@gmail.com>
2019-04-06 21:05:25 +0200

    Dashboard Control added

--- src/views/WebView.js
+++ src/views/WebView.js
@@ -1,20 +1,15 @@
 import React, { Component} from 'react';
 import PersonaToggle from '../components/PersonaToggle.js';
 import Text from '../components/Text.js';
+import DashboardControl from "../components/DashboardControl.js"
 
 class WebView extends React.Component {
   
     render() {
         return(
           <div className="web-app">
-            <div className="dashboard-Rhythmic">
-            <PersonaToggle />
-            </div>
-
-          
-            <div className="bimodal-dash"></div>
-            <Text className="heading" text="How much distraction free time do you need?" />
-
+        
+            <DashboardControl />
 
           </div>
         );
