84b04c67f08486123d3dd716f22786ddf7965747
konradgryt <konrad.gryt@gmail.com>
2019-04-06 23:11:03 +0200

    user agent

--- src/App.js
+++ src/App.js
@@ -5,8 +5,7 @@ import WebView from './views/WebView';
 
 class App extends Component {
     render() {
-    //navigator.userAgent === "TODO add raspberry pi user agent"
-      if (true) {
+      if (navigator.userAgent === "Mozilla/5.0 (X11; Linux armv7l) AppleWebKit/537.36(KHTML, like Gecko) Raspbian Chromium/72.0.3626.121 Chrome/72.0.3626.121 Safari/537.36") {
         return (
           <PiView />
         )
