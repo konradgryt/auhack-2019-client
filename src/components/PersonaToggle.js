9c56caaa32ad787b5c0d80bae3caa9fac4ee8ccd
Nathália Novais <nathnovais@gmail.com>
2019-04-07 03:34:08 +0200

    merge

--- src/components/PersonaToggle.js
+++ src/components/PersonaToggle.js
@@ -17,7 +17,7 @@ class PersonaToggle extends Component {
       <div className="toggle-container">
       <label>
         <span className="toggle-name">Create a routine</span>
-        <Switch width={250} height={15} checkedIcon={false} uncheckedIcon={false} offColor={'#FF7F00'} onColor={'#41E8B6'} onChange={this.handleChange} checked={this.state.checked} />
+        <Switch width={250} height={15} checkedIcon={false} uncheckedIcon={false} offColor={'#EA2356'} onColor={'#ffffff'} onChange={this.handleChange} checked={this.state.checked} />
         <span className="toggle-name">Be in control</span>
       </label>
       </div>
